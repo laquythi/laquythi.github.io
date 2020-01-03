819a88fa646b9d84455d63403aec5d0c9b89050f
Administrator <laquythia4>
2020-01-03 21:03:38 +0700

    delete-hello-next

--- hello-next/pages/about.js
+++
@@ -1,9 +0,0 @@
-import MainLayout from "../components/main-layout";
-
-const About = () => (
-  <MainLayout>
-    <p>render something about my page here</p>
-  </MainLayout>
-);
-
-export default About;
