b03dc0b758d3b86213292fee135087c353c48992
Administrator <laquythia4>
2020-02-25 18:18:26 +0700

    tour of heroes

---
+++ Angular-8/Docs/Tour-of-Heroes/src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import {Hero} from './hero';
+
+export const HEROES : Hero[] = [
+  { id : 11, name : 'Dr Nice'},
+  { id : 12, name : 'Dr Taco'},
+  { id : 13, name : 'Dr Convert'},
+  { id : 14, name : 'Dr Palladium'},
+  { id : 15, name : 'Dr Val'},
+  { id : 16, name : 'Dr Roftbot'},
+  { id : 17, name : 'Dr Dee'},
+  { id : 18, name : 'Dr Tornado'}
+];
