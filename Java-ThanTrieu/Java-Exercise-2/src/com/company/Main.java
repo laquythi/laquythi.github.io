1477ec33d020b682f82ce74419ce1f1f666e7343
thilq <laquythi@gmail.com>
2020-08-10 20:31:29 +0700

    add exercise 2

---
+++ Java-ThanTrieu/Java-Exercise-2/src/com/company/Main.java
@@ -0,0 +1,80 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // 1 : nhap vao 1 mang bat ky co n phan tu,sau do kiem tra mang do co phai la mang doi xung hay ko?
+//        int[] arr;
+//        int n;
+//        System.out.print("moi ban nhap so phan tu cua mang : ");
+//        Scanner sc = new Scanner(System.in);
+//        n = sc.nextInt();
+//        arr = new int[n];
+//        System.out.print("so phan tu cua mang la : " + n + "\n");
+//        for(int i = 0;i < arr.length;i++){
+//            System.out.println("moi ban nhap phan tu thu " + i);
+//            arr[i] = sc.nextInt();
+//        }
+//        System.out.println("mang ban vua nhap la : " + Arrays.toString(arr));
+
+        // kiem tra mang co doi xung ko?
+//        int m = arr.length;
+//        int count = 0;
+//        for(int i = 0;i < m;i++){
+//            if(arr[i] == arr[m - i - 1] ){
+//                count++;
+//            }
+//        }
+
+//        if(count == m){  // 1 4 1
+//            System.out.println("mang ban vua nhap la mang doi xung");
+//        }else{
+//            System.out.println("mang ban vua nhap ko doi xung");
+//        }
+
+        // 2 : nhap 1 mang a co n phan tu. dem so lan xuat hien cua tung phan tu trong mang
+        // in ra phan tu xuat hien nhieu nhat + so lan xuat hien.
+
+//        int[] arr;
+//        int n;
+//        System.out.print("moi ban nhap so phan tu cua mang : ");
+//        Scanner sc = new Scanner(System.in);
+//        n = sc.nextInt();
+//        arr = new int[n];
+//        System.out.print("so phan tu cua mang la : " + n + "\n");
+//        for(int i = 0;i < arr.length;i++){
+//            System.out.println("moi ban nhap phan tu thu " + i);
+//            arr[i] = sc.nextInt();
+//        }
+//        System.out.println("mang ban vua nhap la : " + Arrays.toString(arr));
+//
+//        int count = 0;
+//        int temp = 0;
+//        for(int i = 0;i< arr.length;i++){
+//            for(int j = 0;j < arr.length;j++){
+//                if(arr[j] > arr[i]){
+//                    temp = arr[j];
+//                    arr[j] = arr[i];
+//                    arr[i] = temp;
+//                }
+//            }
+//        }
+//        System.out.println("so lon nhat trong mang la : " + temp);
+//        for(int i = 0;i < arr.length;i++){
+//            if(arr[i] == temp){
+//                count++;
+//            }
+//        }
+//        System.out.println(temp + " xuat hien " + count + " lan trong mang");
+
+        // 3: doc chua hieu de bai lam
+
+        // 4 : tao mang a co n phan tu,sap xep mang a theo thu tu tang dan,loai bo bot cac ptu trung nhau sao cho
+        // moi gia tri ptu chi xuat hien 1 lan trong mang
+        
+    }
+}
--- Java-ThanTrieu/Mang2Chieu-35/src/com/company/Main.java
+++ Java-ThanTrieu/Mang2Chieu-35/src/com/company/Main.java
@@ -3,15 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int[][] arr = new int[4][5];
-        boolean[][] booleans;
-        String[][] strings;
-
-        int[][] arr2 = {
-                {4,7,2,7},
-                {5,8,2,2},
-                {3,6,1,1}
-        };
+//        int[][] arr = new int[4][5];
+//        boolean[][] booleans;
+//        String[][] strings;
+//
+//        int[][] arr2 = {
+//                {4,7,2,7},
+//                {5,8,2,2},
+//                {3,6,1,1}
+//        };
 //        for(int i=0;i< arr2.length;i++){
 //            for(int j=0;j<arr2[0].length;j++){
 //                System.out.print(arr2[i][j] + "\t");
@@ -19,9 +19,24 @@ public class Main {
 //            System.out.println();
 //        }
 
-        for(int i=0;i< arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                System.out.print(arr[i][j] + "\t");
+//        for(int i=0;i< arr.length;i++){
+//            for(int j=0;j<arr[0].length;j++){
+//                System.out.print(arr[i][j] + "\t");
+//            }
+//            System.out.println();
+//        }
+
+        // su dung foreach de truy xuat mang 2 chieu
+        int[][] arr = {
+                {4,5,6,7},
+                {1,6,2,3},
+                {9,3,7,8},
+                {5,7,2,3}
+        };
+        System.out.println("mang 2 chieu truy xuat bang foreach : ");
+        for (int[] x : arr) {
+            for (int y:x){
+                System.out.print(y + " ");
             }
             System.out.println();
         }
