ae1e15d3fd902b839b331399260d7728b42e02cd
thilq <laquythi@gmail.com>
2020-08-26 23:08:56 +0700

    review 58-59 java unica

--- Java-Unica/BaiTapRenLuyenArrayList-58/src/com/company/Main.java
+++ Java-Unica/BaiTapRenLuyenArrayList-58/src/com/company/Main.java
@@ -12,18 +12,18 @@ public class Main {
     }
 
     public static void menu(){
-        System.out.println("1.Them : ");
-        System.out.println("2.Xuat danh sach : ");
-        System.out.println("3.Sua : ");
-        System.out.println("4.Xoa : ");
-        System.out.println("5.Tim kiem : ");
-        System.out.println("6.Sap xep : ");
-        System.out.println("7.Thoat : ");
-        System.out.println("moi ban chon Menu : ");
+        System.out.println();
+        System.out.println("1 Them ");
+        System.out.println("2 Xuat danh sach ");
+        System.out.println("3 Sua danh sach ");
+        System.out.println("4 Xoa ");
+        System.out.println("5 Tim kiem ");
+        System.out.println("6 Sap xep ");
+        System.out.println("7 Thoat ");
+        System.out.println("moi ban chon menu : ");
         int chon = new Scanner(System.in).nextInt();
-
         switch (chon){
-            case 1:
+            case 1 :
                 them();
                 break;
             case 2 :
@@ -45,64 +45,75 @@ public class Main {
                 System.err.println("cam on ban");
                 System.exit(0);
                 break;
+
         }
     }
 
     public static void sapXep() {
-//        System.out.println("array list sau khi sap xep tang dan la : ");
+        System.out.println("\narray list sau khi sap xep tang dan la : ");
         Collections.sort(dsData);
-//        xuatDanhSach();
+        for (int i = 0; i < dsData.size(); i++) {
+            System.out.print(dsData.get(i) + " ");
+        }
 
-        System.out.println("array lis sau khi sap xep giam dan la : ");
-        for (int i = dsData.size()-1;i >=0;i--) {
+        System.out.println("array list sau khi sap xep giam dan la : ");
+        for (int i = dsData.size() - 1; i >= 0 ; i--) {
+            System.out.print(dsData.get(i) + " ");
+        }
+    }
+
+    public static void timKiem() {
+        System.out.println("moi ban nhap vao gia tri can tim : ");
+        int soCanTim = new Scanner(System.in).nextInt();
+
+        // tim kiem cach 1,dung vong lap de tim phan tu nhu binh thuong
+//        int count = 0;
+//        for (int i = 0; i < dsData.size(); i++) {
+//            if (soCanTim == dsData.get(i)){
+//                count++;
+//            }
+//        }
+//        if (count > 0) {
+//            System.out.print("so " + soCanTim + " xuat hien " + count + " lan trong array list");
+//        }else{
+//            System.out.print("so " + soCanTim + " ko xuat hien trong array list");
+//        }
+
+        // tim kiem cach 2,dung contains de kiem tra phan tu trong array list
+        if(dsData.contains(soCanTim)){
+            System.out.print(soCanTim + " co xuat hien trong array list");
+        }else{
+            System.out.print(soCanTim + " ko xuat hien trong array list");
+        }
+    }
+
+    public static void xoaDanhSach() {
+        System.out.println("moi ban nhap vi tri can xoa : ");
+        int viTriXoa = new Scanner(System.in).nextInt();
+        dsData.remove(viTriXoa);
+
+        System.out.println("danh sach sau khi xoa bot phan tu la : ");
+        for (int i = 0; i < dsData.size(); i++) {
+            System.out.print(dsData.get(i) + " ");
+        }
+    }
+
+    public static void suaDanhSach() {
+        System.out.println("moi ban nhap vi tri can sua : ");
+        int viTri = new Scanner(System.in).nextInt();
+        System.out.println("moi ban nhap gia tri can sua : ");
+        int value = new Scanner(System.in).nextInt();
+        dsData.set(viTri,value);
+
+        System.out.println("danh sach sau khi sua lai la : ");
+        for (int i = 0; i < dsData.size(); i++) {
             System.out.print(dsData.get(i) + " ");
         }
         System.out.println();
     }
 
-    public static void timKiem() {
-        System.out.println("nhap vao so ban tim kiem : ");
-        int soCanTim = new Scanner(System.in).nextInt();
-        // tim kiem cach 1
-//        int dem = 0;
-//        for (int i = 0; i < dsData.size(); i++) {
-//            if(soCanTim == dsData.get(i)){
-//                dem++;
-//            }
-//        }
-//        if (dem > 0){
-//            System.out.println("so " + soCanTim + " co xuat hien trong array list");
-//        }else{
-//            System.out.println("so " + soCanTim + " ko xuat hien trong array list");
-//        }
-
-        // tim kiem cach 2,dung contains de kiem tra soCanTim trong list dsData.
-        if(dsData.contains(soCanTim)){
-            System.out.println(soCanTim + " co xuat hien trong array list");
-        }else{
-            System.out.println(soCanTim + " ko xuat hien trong array list");
-        }
-    }
-
-    public static void xoaDanhSach() {
-        System.out.println("nhap vi tri ban muon xoa phan tu : ");
-        int viTriXoa = new Scanner(System.in).nextInt();
-        dsData.remove(viTriXoa);
-        System.out.println("array list sau khi xoa phan tu la : ");
-        xuatDanhSach();
-    }
-
-    public static void suaDanhSach() {
-        System.out.println("nhap vi tri ban muon sua : ");
-        int viTri = new Scanner(System.in).nextInt();
-        System.out.println("nhap gia tri ban muon sua : ");
-        int vaLue = new Scanner(System.in).nextInt();
-        dsData.set(viTri,vaLue);
-        System.out.println("array list sau khi sua la : ");
-        xuatDanhSach();
-    }
-
     public static void xuatDanhSach() {
+        System.out.println("danh sach cac gia tri dc them la : ");
         for (int i = 0; i < dsData.size(); i++) {
             System.out.print(dsData.get(i) + " ");
         }
@@ -110,7 +121,7 @@ public class Main {
     }
 
     public static void them() {
-        System.out.println("moi ban nhap gia tri muon them : ");
+        System.out.println("moi ban nhap gia tri can them : ");
         int value = new Scanner(System.in).nextInt();
         dsData.add(value);
     }
