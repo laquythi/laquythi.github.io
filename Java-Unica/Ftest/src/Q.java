bb180f2586b572d1a1f6904ae1d332c8d787b6da
thilq <laquythi@gmail.com>
2020-10-29 22:26:28 +0700

    remove note

---
+++ Java-Unica/Ftest/src/Q.java
@@ -0,0 +1,12 @@
+public class Q {
+    static int i;
+    public static void main(String[] args) {
+//        int Int = 14;
+//        System.out.println(Int);
+//        System.out.println(12 | 24);
+
+        int i = 14;
+        String a = (i % 2 == 0) ? "so chan" : "so le";
+        System.out.println(a);
+    }
+}
